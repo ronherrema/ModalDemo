24ef22f22e559f57f528f0e9842d6c81a8e903f8
Ron Herrema <rherrema@gmail.com>
2019-11-12 00:15:21 +0000

    Initial Commit

---
+++ ModalDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModalDemo
+//
+//  Created by Ron Herrema on 02/11/2019.
+//  Copyright © 2019 Ron Herrema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    @IBAction func dismissPopup(_ unwindSegue: UIStoryboardSegue) {}
+
+}
+
